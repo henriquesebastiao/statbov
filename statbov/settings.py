079eace7a54e602f976a2d41171b7fb1501ba771
Henrique Sebastião <contato@henriquesebastiao.com>
2024-01-31 10:12:29 -0400

    🪛 fix: Small adjustment on the frontend

--- statbov/settings.py
+++ statbov/settings.py
@@ -137,9 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Adiciona classes do Bootstrap nas mensagens do Django
 MESSAGE_TAGS = {
-    constants.DEBUG: 'alert alert-primary',
-    constants.ERROR: 'alert alert-danger',
-    constants.INFO: 'alert alert-info',
-    constants.SUCCESS: 'alert alert-success',
-    constants.WARNING: 'alert alert-warning',
+    constants.DEBUG: 'alert alert-primary user-select-none',
+    constants.ERROR: 'alert alert-danger user-select-none',
+    constants.INFO: 'alert alert-info user-select-none',
+    constants.SUCCESS: 'alert alert-success user-select-none',
+    constants.WARNING: 'alert alert-warning user-select-none',
 }
