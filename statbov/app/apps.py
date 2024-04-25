8f1f9161126f45f97ccc59db370e8f33110120a4
Henrique Sebastião <contato@henriquesebastiao.com>
2024-04-25 06:49:59 -0400

    🔄 Update project

--- statbov/app/apps.py
+++ statbov/app/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class StatbovAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    verbose_name = 'Statbov'
     name = 'statbov.app'
