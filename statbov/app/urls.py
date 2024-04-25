866058bb9007cb0ccfbda86de0aeeda240bf2c7c
Henrique Sebastião <contato@henriquesebastiao.com>
2024-04-25 06:52:22 -0400

    📰 feat: Add new views to lading page

--- statbov/app/urls.py
+++ statbov/app/urls.py
@@ -5,6 +5,24 @@ from .views import LoginCreateView, LoginView, SignupView, UserCreateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path(
+        'plans/',
+        TemplateView.as_view(template_name='plans.html'),
+        name='plans',
+    ),
+    path(
+        'blog/', TemplateView.as_view(template_name='blog.html'), name='blog'
+    ),
+    path(
+        'support/',
+        TemplateView.as_view(template_name='support.html'),
+        name='support',
+    ),
+    path(
+        'about/',
+        TemplateView.as_view(template_name='about.html'),
+        name='about',
+    ),
     path('signup/', SignupView.as_view(), name='signup'),
     path('signup/user_create/', UserCreateView.as_view(), name='user_create'),
     path('login/', LoginView.as_view(), name='login'),
